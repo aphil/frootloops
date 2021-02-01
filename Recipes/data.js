092989f60fcb0e4411dcb035bd7d547abdfceb5e
André-Philippe Racine <aphil.racine@gmail.com>
2021-01-31 21:22:53 -0500

    add fetcher api, delete and ui

--- Recipes/data.js
+++ Recipes/data.js
@@ -4,26 +4,44 @@ const indexName = "frootloops-fr";
 
 class RecipeRepository {
     async GetAll() {
-        // let recipesResponse = (await client.search({
-        //     size: 50,
-        //     index: indexName,
-        //     body: {
-        //       query: {
-        //         bool: {
-        //           filter: {
-        //             match: { fl_type: "Recipe" },
-        //           },
-        //         },
-        //       }
-        //     },
-        //   }));
-        
-        //   let recipes = recipesResponse.body.hits.hits;
+        let recipesResponse = (await client.search({
+            size: 50,
+            index: indexName,
+            body: {
+                query: {
+                    bool: {
+                        filter: {
+                            match: { fl_type: "Recipe" },
+                        },
+                    },
+                }
+            },
+        }));
 
-        let recipes = [
-            { id: "a", name: "courges" }
-        ];
+        let recipes = recipesResponse.body.hits.hits.map(hit => {
+            return {
+                name: hit._source.name,
+                ingredients: hit._source.ingredients,
+                sourceUrl: hit._source.url
+            }
+        });
+
         return recipes
+    }
+
+    async DeleteAll() {
+        client.deleteByQuery({
+            index: indexName,
+            body: {
+                query: {
+                    bool: {
+                        filter: {
+                            match: { fl_type: "Recipe" },
+                        },
+                    },
+                }
+            }
+        });
     }
 }
 
