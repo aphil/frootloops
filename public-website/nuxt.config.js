23c985e53d25e2583ba23e4853dd67def06b538d
André-Philippe Racine <aphil.racine@gmail.com>
2021-03-07 15:15:34 -0500

    add public website with nuxt and vuetify

---
+++ public-website/nuxt.config.js
@@ -0,0 +1,86 @@
+import colors from 'vuetify/es5/util/colors'
+
+export default {
+  server: {
+    port: 4000
+  },
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    titleTemplate: '%s - public-website',
+    title: 'public-website',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/typescript
+    '@nuxt/typescript-build',
+    // https://go.nuxtjs.dev/vuetify
+    '@nuxtjs/vuetify',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/pwa
+    '@nuxtjs/pwa',
+  ],
+
+  // PWA module configuration: https://go.nuxtjs.dev/pwa
+  pwa: {
+    manifest: {
+      lang: 'en'
+    }
+  },
+
+  // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
+  vuetify: {
+    //customVariables: ['~/assets/variables.scss'],
+    theme: {
+      dark: false,
+      light: true,
+      themes: {
+        dark: {
+          primary: '#E53935',
+          accent: '#FF4081',
+          secondary: '#ffe18d',
+          success: '#4CAF50',
+          info: '#2196F3',
+          warning: '#FB8C00',
+          error: '#FF5252'
+        },
+        light: {
+          primary: '#EC407A',
+          accent: '#e91e63',
+          secondary: '#30b1dc',
+          success: '#4CAF50',
+          info: '#2196F3',
+          warning: '#FB8C00',
+          error: '#FF5252'
+        }
+      }
+    }
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
---
+++ public-website/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-layout row wrap>
+    <a href="https://www.google.com">This is a link!</a>
+    <!-- <v-card
+      v-for="recipe in recipes"
+      :key="recipe.id"
+      class="mx-auto my-12"
+      max-width="374"
+    >
+      <v-card-title>{{ recipe.name }}</v-card-title>
+      <v-img :src="recipe.imageUrl" height="250px"></v-img>
+    </v-card> -->
+  </v-layout>
+</template>
+
+<script>
+export default {};
+</script>
